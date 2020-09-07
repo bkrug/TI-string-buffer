0ead12d6424fadb69d4e48377a1cd319f55214a7
Benjamin Krug <benjaminkrug@yahoo.com>
2020-09-07 03:32:54 -0400

    Use xax99 for array files

--- assm.bat
+++ assm.bat
@@ -3,6 +3,9 @@ xas99.py -C -R MEMLOAD.asm -L MEMLOAD.lst
 xas99.py -C -R MEMTST.asm -L MEMTST.lst
 xas99.py -C -R TESTFRAM.asm -L TESTFRAM.lst
 xas99.py -C -R VAR.asm -L VAR.lst
+xas99.py -C -R ARRAY.asm -L ARRAY.lst
+xas99.py -C -R ARRYLOAD.asm -L ARRYLOAD.lst
+xas99.py -C -R ARRYTST.asm -L ARRYTST.lst
 if exist .\work.dsk delete work.dsk
 xdm99.py -X sssd work.dsk
 xdm99.py work.dsk -a MEMBUF.obj -f DIS/FIX80
@@ -10,3 +13,6 @@ xdm99.py work.dsk -a MEMLOAD.obj -f DIS/FIX80
 xdm99.py work.dsk -a MEMTST.obj -f DIS/FIX80
 xdm99.py work.dsk -a TESTFRAM.obj -f DIS/FIX80
 xdm99.py work.dsk -a VAR.obj -f DIS/FIX80
+xdm99.py work.dsk -a ARRAY.obj -f DIS/FIX80
+xdm99.py work.dsk -a ARRYLOAD.obj -f DIS/FIX80
+xdm99.py work.dsk -a ARRYTST.obj -f DIS/FIX80
