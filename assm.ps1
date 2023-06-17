562fb011647c73e1edb58fbe8c99ad69e775c91e
Benjamin Krug <benjaminkrug@yahoo.com>
2023-06-17 09:01:48 -0400

    Create object files useable by XDT99

--- assm.ps1
+++ assm.ps1
@@ -15,11 +15,11 @@ ForEach($file in $files) {
 
 # Add version information to some files
 Add-Content -Path .\MEMBUF.obj -NoNewline -Value `
-': Memory Management - Chunk Buffer                                              : Version 1.2.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
+': Memory Management - Chunk Buffer                                              : Version 1.3.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
 Add-Content -Path .\ARRAY.obj -NoNewline -Value `
-': Memory Management - Arrays                                                    : Version 1.2.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
+': Memory Management - Arrays                                                    : Version 1.3.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
 Add-Content -Path .\VAR.obj -NoNewline -Value `
-': Memory Management - library non-static memory                                 : Version 1.2.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
+': Memory Management - library non-static memory                                 : Version 1.3.0                                                                 : https://github.com/bkrug/TI-string-buffer                                     '
 
 # Add some files to a Disk image
 $diskName = 'BufferAndArray.dsk'
@@ -31,8 +31,14 @@ xdm99.py $diskName -a 'MEMBUF.obj' -f DIS/FIX80
 xdm99.py $diskName -a 'ARRAY.obj' -f DIS/FIX80
 xdm99.py $diskName -a 'VAR.obj' -f DIS/FIX80
 
-# Add TIFILES header to all object files
-$objectFiles = Get-ChildItem ".\" -Filter *.obj
+# Create a version of the object files that can be used by XDT99
+Copy-Item 'MEMBUF.obj' 'MEMBUF.noheader.obj'
+Copy-Item 'ARRAY.obj' 'ARRAY.noheader.obj'
+Copy-Item 'VAR.obj' 'VAR.noheader.obj'
+
+# Add TIFILES header to most object files
+$objectFiles = Get-ChildItem ".\" -Filter *.obj |
+               Where-Object { $_.Name.EndsWith('.noheader.obj') -ne 1 }
 ForEach($objectFile in $objectFiles) {
     xdm99.py -T $objectFile.Name -f DIS/FIX80 -o $objectFile.Name
 }
